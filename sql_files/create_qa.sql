997088cadcb21ea2575ca769269fe9b352f1146f
enjoymrsun <derek_x_sun@163.com>
2019-04-06 21:06:46 -0400

    update sql file

---
+++ sql_files/create_qa.sql
@@ -0,0 +1,118 @@
+SHOW DATABASES;
+
+DROP DATABASE IF EXISTS qa;
+CREATE DATABASE lotrsunx;
+
+SHOW DATABASES;
+
+ALTER DATABASE lotrsunx CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE lotrsunx;
+
+DROP TABLE IF EXISTS lotr_species, lotr_books, lotr_regions;
+DROP TABLE IF EXISTS lotr_characters;
+DROP TABLE IF EXISTS lotr_firstencounters;
+
+SHOW TABLES;
+
+CREATE TABLE qa_users (
+	user_id		INT UNSIGNED	NOT NULL	AUTO_INCREMENT,
+	username	VARCHAR(32)   	NOT NULL,
+	password	VARCHAR(64)	
+    email       VARCHAR(32)			NULL,
+    phone   
+    is_active   TINYINT     active or not or ban for unrealateed saying
+    reg_time    INT  register time
+    user_pic    VARCHAR(128) avator
+	size			INT UNSIGNED	NOT NULL,
+	PRIMARY KEY (user_id),
+	UNIQUE INDEX (name)
+);
+DESCRIBE lotr_species;
+
+
+CREATE TABLE qa_questions (
+	question_id		INT UNSIGNED	NOT NULL	AUTO_INCREMENT,
+    title  VARCHAR(128) question title
+    desc   TEXT
+    cat_id  int  category of questions
+	topic_id int  topics belong of this questions
+    user_id  int who ask this question
+    pub_time    public time
+    focus_num  int
+    reply_num   int
+    view_num    INT
+);
+DESCRIBE lotr_books;
+
+
+CREATE TABLE qa_categories (
+	question_id		INT UNSIGNED	NOT NULL	AUTO_INCREMENT,
+    title  VARCHAR(128) question title
+    desc   TEXT
+    cat_id  int  category of questions
+	topic_id int  topics belong of this questions
+    user_id  int who ask this question
+    pub_time    public time
+    focus_num  int
+    reply_num   int
+    view_num    INT
+);
+
+CREATE TABLE lotr_regions (
+	regions_id				INT UNSIGNED	NOT NULL	AUTO_INCREMENT,
+	name					VARCHAR(64)		NOT NULL,
+	major_species			VARCHAR(64)		NOT NULL,
+	description				TEXT			NULL,
+	middle_earth_location	VARCHAR(128)	NOT NULL	DEFAULT '',
+	leader					VARCHAR(64)		NOT NULL	DEFAULT 'none',
+	PRIMARY KEY (regions_id),
+	FOREIGN KEY (major_species) REFERENCES lotr_species (name)
+		ON DELETE RESTRICT ON UPDATE CASCADE,
+	UNIQUE INDEX (name)
+);
+DESCRIBE lotr_regions;
+
+
+CREATE TABLE lotr_characters (
+	characters_id				INT UNSIGNED		NOT NULL	AUTO_INCREMENT,
+	name						VARCHAR(64)			NOT NULL,
+	species						VARCHAR(64)			NOT NULL,
+	homeland					VARCHAR(64)			NOT NULL,
+	royalty						BOOLEAN				NOT NULL,
+	fellowship					BOOLEAN				NOT NULL,
+	survive						BOOLEAN				NOT NULL,
+	alias						VARCHAR(256)		NOT NULL	DEFAULT '',
+	book_number_introduction 	TINYINT UNSIGNED	NOT NULL,
+	PRIMARY KEY (characters_id),
+	FOREIGN KEY (species) 					REFERENCES lotr_species (name)
+		ON DELETE RESTRICT 	ON UPDATE CASCADE,
+	FOREIGN KEY (homeland) 					REFERENCES lotr_regions (name)
+		ON DELETE RESTRICT 	ON UPDATE CASCADE,
+	FOREIGN KEY (book_number_introduction)	REFERENCES lotr_books (number)
+		ON DELETE RESTRICT	ON UPDATE RESTRICT,
+	UNIQUE INDEX (name)
+);
+DESCRIBE lotr_characters;
+
+
+CREATE TABLE lotr_firstencounters (
+	firstencounters_id	INT UNSIGNED		NOT NULL	AUTO_INCREMENT,
+	character1			VARCHAR(64)			NOT NULL,
+	character2			VARCHAR(64)			NOT NULL,
+	book				TINYINT UNSIGNED	NOT NULL,
+	location     		VARCHAR(64)			NOT NULL,
+	PRIMARY KEY (firstencounters_id),
+	FOREIGN KEY (character1)	REFERENCES lotr_characters (name)
+		ON DELETE CASCADE	ON UPDATE CASCADE,
+	FOREIGN KEY (character2)	REFERENCES lotr_characters (name)
+		ON DELETE CASCADE	ON UPDATE CASCADE,
+	FOREIGN KEY (book) 			REFERENCES lotr_books (number)
+		ON DELETE RESTRICT 	ON UPDATE RESTRICT,
+	FOREIGN KEY (location) 		REFERENCES lotr_regions (name)
+		ON DELETE RESTRICT 	ON UPDATE CASCADE
+);
+DESCRIBE lotr_firstencounters;
+
+
+SHOW TABLES;
